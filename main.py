f657956d57ee55b1aabe4a7282e0ad1adf5f6ed7
Sarah Borges de Oliveira dos Santos <sarahsantos@MacBook-Air-de-Sarah.local>
2022-05-05 19:41:08 -0300

    Criacão das funcoes e teste de unidade da soma e divisão

---
+++ main.py
@@ -0,0 +1,28 @@
+import pytest
+
+def imprimir_oi(nome):
+    print(f'Oi, {nome}')
+
+def somar(numero_a, numero_b):
+    return numero_a + numero_b
+
+
+if __name__ == '__main__':
+    imprimir_oi('Sarah')
+
+    resultado = somar(7, -6)
+    print(f'A soma: {resultado}')
+
+
+def testar_somar():
+    # 1 - Configura
+    numero_a = 8
+    numero_b = 7
+    resultado_esperado = 15
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
+        
+    # 3 - Valida
+    assert resultado_esperado == resultado_obtido
+
