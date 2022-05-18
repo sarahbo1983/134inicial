ec23ee97d0519874d0e291ae9e17db1548ac35d5
sarahbo1983 <sarahbo1983@gmail.com>
2022-05-18 20:46:47 -0300

    Teste da API da Petstore, da entidade Pet com o metodo Post

---
+++ tests/api/test_pet.py
@@ -0,0 +1,39 @@
+# bibliotecas
+import pytest
+import requests
+
+# variaveis publicas
+url = 'https://petstore.swagger.io/v2/pet'
+headers = {'Content-Type': 'application/json'}
+
+
+# definições das funções (defs)
+
+def teste_incluir_pet():
+    # Configura / Prepara
+    # Dados de entrada provem do pet1.json
+
+    # Resultados Esperados
+    status_code_esperado = 200
+    pet_id_esperado = 3269095
+    pet_nome_esperado = "Snoopy"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_nome_tag_esperado = "vacinado"
+
+    # Executa
+    resultado_obtido = requests.post(
+        url=url,
+        headers=headers,
+        data=open('/Users/sarahsantos/PycharmProjects/134inicial/vendors/json/pet1.json')
+    )
+
+    # Valida
+    print(resultado_obtido)
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(corpo_do_resultado_obtido)
+
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido['id'] == pet_id_esperado
+    assert corpo_do_resultado_obtido['name'] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_nome_tag_esperado
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -49,6 +49,17 @@ def teste_subtrair():
     assert resultado_obtido == resultado_esperado
 
 
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', ler_csv('/Users/sarahsantos/PycharmProjects/134inicial/vendors/csv/massa_teste_subtrair_positivo.csv'))
+def teste_subtrair_leitura_csv(numero_a, numero_b, resultado_esperado):
+    # 1 - Prepara / Configura
+
+    # 2 - Executa
+    resultado_obtido = subtrair(int(numero_a), int(numero_b))
+
+    # 3 - Valida / Checa
+    assert resultado_obtido == int(resultado_esperado)
+
+
 def teste_multiplicar():
     # 1 - Configura
     numero_a = 3
